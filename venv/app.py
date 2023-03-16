c1703d33a03a5d78fa12ed916aabca5466156d35
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-03-16 11:33:18 +1100

    added population data to countries, added users table

---
+++ venv/app.py
@@ -0,0 +1,17 @@
+import json 
+from flask import Flask 
+import psycopg2 
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return "Welcome to the LGBTQI+ Safety Travel API"
+
+
+
+if __name__ == '__main__':
+    app.run()
